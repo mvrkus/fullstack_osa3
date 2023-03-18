18a594b6597ad3a97dcb2e63e0e51a4203abad69
mvrkus <markus.tarkkala@gmail.com>
2023-03-18 16:57:39 +0200

    3.5

--- index.js
+++ index.js
@@ -60,6 +60,35 @@ app.get('/api/persons/:id', (req, res) => {
     res.json(person)
 })
 
+/app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id)
+  persons = persons.filter(person => person.id !== id)
+
+  res.status(204).end()
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body
+  console.log(body);
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'sisältö puuttuu'
+    })
+  }
+  
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 1000000)
+  }
+  console.log(person.id);
+  
+  persons = persons.concat(person)
+
+  res.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
