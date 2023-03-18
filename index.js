1efec8ce15fdb151eb3aeded77da3b8eda924d72
mvrkus <markus.tarkkala@gmail.com>
2023-03-18 15:38:25 +0200

    3.3

--- index.js
+++ index.js
@@ -48,6 +48,18 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const person = persons.find(person => person.id === id)
+
+    if (person) {
+        res.json(person)
+    } else {
+        res.status(404).end()
+    }
+    res.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
