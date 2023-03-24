a5d461bd84b1d734748fbb449e55741000e0c634
mvrkus <markus.tarkkala@gmail.com>
2023-03-24 14:17:47 +0200

    3.10

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ const findSameName = (tarkistettava) => {
 } 
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
