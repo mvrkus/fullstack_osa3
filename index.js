ac968729bc615a58fb0d34368f9a33d717224bd8
mvrkus <markus.tarkkala@gmail.com>
2023-03-31 15:00:06 +0300

    3.14.2

--- index.js
+++ index.js
@@ -1,19 +1,39 @@
+require('dotenv').config()
 const { request, response } = require('express')
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 
-app.use(express.json())
+
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :param'))
 app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
 
 morgan.token('param', function(req, res, param) {
   return JSON.stringify(req.body);
 });
 
 
-let persons = [
+const errorHandler = (error, req, res, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return res.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+const unknownEndpoint = (req, res) => {
+  res.status(404).send({ error: 'unknown endpoint'})
+}
+
+
+
+/*let persons = [
     {
         "name": "Dan Abramov",
         "number": "12-43-234345",
@@ -29,36 +49,29 @@ let persons = [
         "number": "22",
         "id": 3
       }
-]
-
-
+]*/
 
 app.get('/info', (req, res) => {
-
-    const ytLkm = persons.length
-    console.log(persons.length);
-    
-    const pyyntoAika = new Date()
-    console.log(pyyntoAika);
-    
-    res.send(`<div>The phonebook has info for ${ytLkm} people</div>
-    <div> ${pyyntoAika}</div>`)
+    res.send(`<div>The phonebook has info for people</div>
+    <div>Tähän pyyntöaika</div>`)
 })
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons =>{
+    res.json(persons)
+  })
 })
 
-app.get('/api/persons/:id', (req, res) => {
-    const id = Number(req.params.id)
-    const person = persons.find(person => person.id === id)
-
+app.get('/api/persons/:id', (req, res, next) => {
+  Person.findById(req.params.id)
+  .then(person => {
     if (person) {
-        res.json(person)
+      res.json(person)
     } else {
-        res.status(404).end()
+      res.status(404).end()
     }
-    res.json(person)
+  })
+  .catch(error => next(error))
 })
 
 /app.delete('/api/persons/:id', (req, res) => {
@@ -70,47 +83,48 @@ app.get('/api/persons/:id', (req, res) => {
 
 app.post('/api/persons', (req, res) => {
   const body = req.body
-  //console.log(body);
-
-  if (!body.name) {
+  
+  if (body.name === undefined) {
     return res.status(400).json({
       error: 'nimi puuttuu'
     })
   }
 
-  if (!body.number) {
+  if (body.number === undefined) {
     return res.status(400).json({
       error: 'numero puuttuu'
     })
   }
 
-  if (findSameName(body.name)) {
+  /*if (findSameName(body.name)) {
     return res.status(400).json({
       error: 'nimi on jo luettelossa'
     })
-  }
+  }*/
   
-  const person = {
+  const person = new Person({
     name: body.name,
     number: body.number,
-    id: Math.floor(Math.random() * 1000000)
-  }
-  //console.log(person.id);
+  })
   
-  persons = persons.concat(person)
+  person.save().then(savedPerson => {
+    res.json(savedPerson)
+  })
 
-  res.json(person)
 })
 
-const findSameName = (tarkistettava) => {
+/*const findSameName = (tarkistettava) => {
   if (persons.find(person => person.name === tarkistettava)) {
     return true
   }
   return false
-} 
+}*/
+
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
